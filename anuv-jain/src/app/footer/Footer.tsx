d2d9018f34b280ed9359621029379ac8fecdcb9b
nirbhayrana123 <nirbhydesign@gmail.com>
2025-11-25 12:29:40 +0530

    ab

--- anuv-jain/src/app/footer/Footer.tsx
+++ anuv-jain/src/app/footer/Footer.tsx
@@ -22,28 +22,28 @@ export default function Footer() {
       </form>
       <div className="social-icons">
         <a href="#">
-          <img src="images/ins (1).png" alt="instagram" />
+          <img className='spotify3' src="images/ins (1).png" alt="instagram" />
           <svg className="draw-circle" viewBox="0 0 120 120">
             <path d="M60 10 C88 12 108 32 110 60 C112 88 88 110 60 110 C32 108 12 88 10 60 C12 32 32 12 60 10 Z" />
           </svg>
         </a>
 
         <a href="#">
-          <img src="images/ins (4).png" alt="x" />
+          <img className='x1' src="images/ins (4).png" alt="x" />
           <svg className="draw-circle" viewBox="0 0 120 120">
             <path d="M60 10 C88 12 108 32 110 60 C112 88 88 110 60 110 C32 108 12 88 10 60 C12 32 32 12 60 10 Z" />
           </svg>
         </a>
 
         <a href="#">
-          <img src="images/ins (3).png" alt="youtube" />
+          <img className='youtube2' src="images/ins (3).png" alt="youtube" />
           <svg className="draw-circle" viewBox="0 0 120 120">
             <path d="M60 10 C88 12 108 32 110 60 C112 88 88 110 60 110 C32 108 12 88 10 60 C12 32 32 12 60 10 Z" />
           </svg>
         </a>
 
         <a href="#">
-          <img src="images/ins (2).png" alt="spotify" />
+          <img className='spotify3' src="images/ins (2).png" alt="spotify" />
           <svg className="draw-circle" viewBox="0 0 120 120">
             <path d="M60 10 C88 12 108 32 110 60 C112 88 88 110 60 110 C32 108 12 88 10 60 C12 32 32 12 60 10 Z" />
           </svg>
